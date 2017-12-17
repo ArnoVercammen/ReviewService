cb4eaf12ad4ca5e0a47635fe1cad498b71a558ca
Arno Vercammen <r0632439@ucll.be>
2017-12-17 22:58:02 +0100

    zefzefzef

---
+++ src/main/docker/run.sh
@@ -0,0 +1,4 @@
+echo "********************************************************"
+echo "Starting Server"
+echo "********************************************************"
+java -jar /usr/local/reviewservice/@project.build.finalName@.jar
